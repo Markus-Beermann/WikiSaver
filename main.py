cd732912954b831359ea1af1e50ea6934780cc17
ateetvatan <ateetph@gmail.com>
2025-02-24 16:10:39 +0100

    Initial skeleton

---
+++ main.py
@@ -0,0 +1,20 @@
+
+def get_random_travel_location():
+    pass
+
+def get_wikipedia_links(title):
+    pass
+
+def calculate_distance(coord1, coord2):
+    pass
+
+def get_openai_travel_advice(location, target):
+    pass
+
+def start_game():
+    pass
+
+if __name__ == "__main__":
+    start_game()
+
+
