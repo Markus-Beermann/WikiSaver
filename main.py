d708b6b4b043613709771baa55980763daded9e9
ateetvatan <ateetph@gmail.com>
2025-02-25 14:18:20 +0100

    Markus Changes integrated.

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 """The WikiSaver Main Module."""
 import random
 from geopy import distance
-from city_locations import CityLocations
+from city_info import CityInfo
 
 
-def get_random_travel_location():
-    locations = CityLocations.locations
+def get_random_travel_location(locations: list):
     return random.choice(locations)
 
 
-def get_travel_location_coordinates(city_name):
+def get_travel_location_coordinates(city_name, locations: list):
     """Function to get the given location coordinates."""
-    locations = CityLocations.locations
     city_dict = dict(x for x in locations if x[0] == city_name)
     return city_dict[city_name]
 
 
-def get_wikipedia_links(city_name) -> list:
+def get_wikipedia_links(city_name, locations: list) -> list:
     """To do. Function needs to be implemented. Now just a fake functionality,
     Here Needs to query Wiki API to get a city list from wiki page.
     To do. Function Features:
@@ -24,8 +22,6 @@ def get_wikipedia_links(city_name) -> list:
     2. Search all the cities on the wiki page.
     3. Create a list like ["Berlin, Germany", "Tokyo, Japan", "New York City, USA"]
      """
-
-    locations = CityLocations.locations
     random_locations = random.sample(locations, 10)
     # ["Berlin, Germany","Tokyo, Japan","New York City, USA"]
     return [x[0] for x in random_locations]
@@ -55,21 +51,23 @@ def get_openai_travel_advice(location, target):
 
 def start_game():
     """Game Loop Method."""
+    city_info = CityInfo()
+    country_data = city_info.fetch_data()
+
     print("\nWelcome to WikiSaver")
-    #Get Random Start and End Locations.
-    start_location, start_cords = get_random_travel_location()
-    target_location, target_cords = get_random_travel_location()
+    # Get Random Start and End Locations.
+    start_location, start_cords = get_random_travel_location(country_data)
+    target_location, target_cords = get_random_travel_location(country_data)
 
     # Ensuring Start location and End location should Not Be the same.
     while start_location == target_location:
-        target_location, target_cords = get_random_travel_location()
-
+        target_location, target_cords = get_random_travel_location(country_data)
 
     print(f"Starting Location: {start_location}")
     print(f"Target Destination: {target_location}")
     print("Travel wisely within budget!")
 
-    #Initialize Budget, Locations and steps
+    # Initialize Budget, Locations and steps
     current_location = start_location
     current_cords = start_cords
     steps = 0
@@ -77,18 +75,16 @@ def start_game():
     budget = None
     initial_budget = None
 
+    while True:  # current_location !=target_location
 
-
-    while True:#current_location !=target_location
-
-        #Calculate distance to target
+        # Calculate distance to target
         distance_to_target = calculate_distance(current_cords, target_cords)
-        #Calculate travel cost
+        # Calculate travel cost
         travel_cost = calculate_cost(distance_to_target)
 
         # setting budget = calculated travel_cost
         if budget is None:
-            #Agssigning enough budget to user
+            # Agssigning enough budget to user
             budget = travel_cost * 5
             initial_budget = budget
 
@@ -98,7 +94,7 @@ def start_game():
         print(f"Distance to Target: {distance_to_target} km")
         print(f"Budget Remaining: ${budget}")
         print("******************************************")
-        links = get_wikipedia_links(current_location)
+        links = get_wikipedia_links(current_location, country_data)
         # Block to get the User Choice.
         while True:
             try:
@@ -107,7 +103,7 @@ def start_game():
                     print(f"{i}. {link}")
                 choice = input("Enter choice or [empty text for more choices / 0 to quit.] ").strip()
                 if choice == "":
-                    links = get_wikipedia_links(current_location)
+                    links = get_wikipedia_links(current_location, country_data)
                     continue  # empty text for more choices
                 if choice == "0":
                     break  # 0 to quit
@@ -119,12 +115,12 @@ def start_game():
             except ValueError:
                 print("Please enter valid choice a number from 1 to 10")
         if choice == "0":
-            break # 0 to quit
+            break  # 0 to quit
 
         next_location = links[choice - 1]
         # Now calculate the travel cost
         # Get current location coordinates
-        next_loc_cords = get_travel_location_coordinates(next_location)
+        next_loc_cords = get_travel_location_coordinates(next_location, country_data)
         distance_to_choice = calculate_distance(current_cords, next_loc_cords)
         travel_cost = calculate_cost(distance_to_choice)
 
